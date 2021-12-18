bbfa1c8aff03780554be73d734ccc6506d4b9b1d
Nikita Lafinskiy <niklafinskiy@gmail.com>
2021-12-18 15:50:56 +0200

    changed the backend server origin

--- pages/auth/logout.js
+++ pages/auth/logout.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import router from 'next/router';
 
 export default function Logout() {
-  console.log('logout2');
+  console.log('logout');
   const num = 1;
   useEffect(() => {
     axios({
