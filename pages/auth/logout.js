1db78d36f46c8c78d663c39e6183a9ff85be573e
Nikita Lafinskiy <niklafinskiy@gmail.com>
2021-12-18 12:41:12 +0200

    getting the server to interact properly

--- pages/auth/logout.js
+++ pages/auth/logout.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import router from 'next/router';
 
 export default function Logout() {
-  console.log('logout');
+  console.log('logout1');
   const num = 1;
   useEffect(() => {
     axios({
